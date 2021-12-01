c1d5f9abc2b7dc4e918100ed645549e804d6a887
sijie he <1274718084@qq.com>
2021-12-01 01:08:38 +0000

    Topic 10 - Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -18,8 +18,16 @@ router.post('/',asyncHandler( async (req, res, next) => {
       return next();
     }
     if (req.query.action === 'register') {
+      let pwdFormat =/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
+      let aa = pwdFormat.test(req.body.password);
+      if (aa == true){
       await User.create(req.body);
       res.status(201).json({code: 201, msg: 'Successful created new user.'});
+    }else {
+      res.status(404).json({ code: 404, msg: 'Bad password' });
+
+    }
+
     } else {
       const user = await User.findByUserName(req.body.username);
         if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
@@ -53,9 +61,14 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
   const userName = req.params.userName;
   const movie = await movieModel.findByMovieDBId(newFavourite);
   const user = await User.findByUserName(userName);
-  await user.favourites.push(movie._id);
-  await user.save(); 
-  res.status(201).json(user); 
+  if (user.favourites.indexOf(movie._id)==-1){
+    await user.favourites.push(movie._id);
+    await user.save(); 
+    res.status(201).json(user); 
+  }else{
+    res.status(404).json({ code: 404, msg: 'Unable to add duplicates' });
+  }
+  
 }));
 router.get('/:userName/favourites', asyncHandler( async (req, res) => {
   const userName = req.params.userName;
